6e73422180dd3a3c3b9c71db3c8d92212fae1407
Jairo Rocha <jairabvl@hotmail.com>
2025-05-21 09:53:14 -0300

    server att

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ app.use(express.json())
 app.use(cors())
 
 const books = [
-    { title: "O senhor ods Aneis", author: "J.R.R. Tolkien" },
-    { title: "Harry Potter", author: "J.K Rowling" },
+    { title: "O senhor dos Aneis", author: "J.R.R. Tolkien" },
+    { title: "Harry Potter", author: "J.K. Rowling" },
     { title: "O poderoso chefão", author: "Mario Puzo" },
     { title: "Nada será como antes", author: "Miguel Nicolelis" },
-    { title: "Cemitério de Dragões", author: "Raphael Draccon" },
+    { title: "Cemiterio de Dragões", author: "Raphael Draccon" }
 ]
 
 app.get('/api/books', (req, res) => {
@@ -22,13 +22,15 @@ app.get('/api/books', (req, res) => {
 })
 
 app.post('/api/books', (req, res) => {
-    const {title, author} = req.body
+    const {tittle, author} = req.body
+
     if (!title || !author) {
         return res.status(400).json({error: "Titulo e autor são obrigatorios."})
     }
 
     const newBook = { title, author }
     books.push(newBook)
+
     res.status(201).json(newBook)
 })
 
@@ -36,8 +38,14 @@ app.delete("api/books/:index", (req, res) => {
     const index = Number.parseInt(req.params.index)
 
     if (isNaN(index) || index < 0 || index >= books.length) {
-        return res.status(404).json({error: "Livro não encontrado"})
+        return res.status(404).json({error: "Livro não encontrado!"})
     }
 
-    const removeBook = books.splice(index)
+    const removeBook = books.splice(index, 1)[0]
+
+    res.json(removeBook)
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor Express rodadno em htpp://localhost:${PORT}`)
 })
